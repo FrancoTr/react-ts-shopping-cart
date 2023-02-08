7b894f095523699ccb001b1ed1d5583d29d4f5ff
Franco Traverso <franco-t20@outlook.com>
2023-02-07 22:49:01 -0300

    Add delete item button to the cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
 import { formatCurrency } from "../utilities/formatCurrency";
@@ -11,7 +11,7 @@ type CartItemProps = {
 export function CartItem({ id, quantity }: CartItemProps) {
   const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((i) => i.id === id);
-  if (item === null) return null;
+  if (item == null) return null;
   return (
     <Stack direction='horizontal' gap={2} className='d-flex align-items-center'>
       <img src={item?.imgUrl} style={{ width: "125px", height: "75px", objectFit: "cover" }} />
@@ -29,6 +29,9 @@ export function CartItem({ id, quantity }: CartItemProps) {
         </div>
       </div>
       <div>{formatCurrency((item?.price as number) * quantity)}</div>
+      <Button variant='outline-danger' size='sm' onClick={() => removeFromCart(item?.id)}>
+        &times;
+      </Button>
     </Stack>
   );
 }
